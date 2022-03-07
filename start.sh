3f992be2918a0072571d239e64fe64de97821e67
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2022-03-07 00:23:18 +0000

    Added sleep to start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 cd /etc/ices2/
 /etc/init.d/icecast2 start && ices2 /etc/ices2/ices-playlist.xml &
 
+sleep 5 # Sleep to give icecast time to boot up
+
 #Relay for webkit clients
 ffmpeg -re -i http://localhost:8000${OGG_STREAM_ENDPOINT} -vn \
 -codec:a libmp3lame -b:a 64k -f mp3 \
